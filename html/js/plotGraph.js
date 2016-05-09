f07bcc325cc8f1bca50bbd6b7b1cb46e0ec24170
Saurabh Deshpande <saurabhd2511@gmail.com>
2016-05-08 20:53:35 -0700

    Added title graph and fixed session problem

--- html/js/plotGraph.js
+++ html/js/plotGraph.js
@@ -1,4 +1,200 @@
-			function openFirewall() {
+//			function openFirewall() {
+//			    document.getElementById("details1").style.display = "block";
+//			    document.getElementById("details2").style.display = "none";
+//			}
+//
+//			function openAuth() {
+//			    document.getElementById("details1").style.display = "none";
+//			    document.getElementById("details2").style.display = "block";
+//			}
+//
+//			var url;
+//
+//			function myFunction() 
+//			{
+//				var chartTitle;
+//			    var common = "http://52.39.5.137:8090";
+//
+//			    var e = document.getElementById("dropdown1");
+//			    var f = document.getElementById("dropdown2");
+//
+//			    if (document.getElementById("details1").style.display == "block") {
+//			        plota = "firewall";
+//			        var plotb = e.options[e.selectedIndex].value;
+//			    } else {
+//			        plota = "auth";
+//			        var plotb = f.options[f.selectedIndex].value;
+//			    }
+//
+//			    var protocol = document.getElementById("ip").value;
+//
+//				if (protocol != "") {
+//			        url = common + "/" + plota + "/" + plotb + "?ip=" + protocol;
+//					sessionStorage.setItem("url",url);
+//			    } else {
+//			        url = common + "/" + plota + "/" + plotb;
+//					sessionStorage.setItem("url",url);
+//			    }
+//			    
+//
+//			    if (plotb == "userActivity") {
+//			        document.getElementById("graph").style.display = "none";
+//			        window.location.href = "userActivity_table.html";
+//			    } else if (plotb == "destUserActivity") {
+//			        document.getElementById("graph").style.display = "none";
+//			        window.location.href = "destUserActivity_table.html";
+//			    } else if (plotb == "destIPActivity") {
+//			        document.getElementById("graph").style.display = "none";
+//			        window.location.href = "destIPActivity_table.html";
+//			    } else if (plotb == "sourceIPActivity") {
+//			        document.getElementById("graph").style.display = "none";
+//			        window.location.href = "sourceIPActivity_table.html";
+//			   	}else if(plotb == "acceptedEvents"){
+//					document.getElementById("graph").style.display = "none";
+//			        window.location.href = "acceptedEvents_table.html";
+//			    } else if(plotb == "rejectedEvents") {
+//			        document.getElementById("graph").style.display = "none";
+//			        window.location.href = "rejectedEvents_table.html";
+//			    }
+//
+//								
+//			    var xhr = new XMLHttpRequest();
+//			    xhr.open("GET", url, false);
+//			    xhr.send();
+//			    JSONdata = xhr.responseText;
+//			    var src = JSON.parse(JSONdata);
+//			    var key;
+//			    var doom = [];
+//			    for (key in src) {
+//			        if (src.hasOwnProperty(key)) {
+//			            doom.push({
+//			                'ip': key,
+//			                'ratings': src[key]
+//			            });
+//			        }
+//			    }
+//
+//			    document.getElementById("graph").style.display = "block";
+//
+//				
+//				
+//				if (plotb == "multipleDestination") {
+//					chartTitle = "Multiple Destinations";
+//			    } else if (plotb == "multipleSource") {
+//			       	chartTitle = "Multiple Sources";
+//			    } else if (plotb == "topDestPorts") {
+//			        chartTitle = "Top Destination Ports";
+//			    } else if (plotb == "topSourcePorts") {
+//			       chartTitle = "Top Source Ports";
+//			    } else if (plotb == "topDestinations") {
+//			        chartTitle = "Top Destinations Visited";
+//			    } else if (plotb == "failedLogin") {
+//			       chartTitle = "Failed Logins";
+//			    } else if (plotb == "remoteLogin") {
+//			        chartTitle = "Remote Login";
+//			    }
+//
+//			
+//				
+//			    
+//			    var processed_json = new Array();
+//
+//
+//			    var processed_json = new Array();
+//			    $.map(doom, function(obj, i) {
+//			        processed_json.push([obj.ip, parseInt(obj.ratings)]);
+//			    });
+//
+//
+//			    BarChart();
+//
+//
+//				function BarChart() {
+//			    var chart = new Highcharts.Chart({
+//			        chart: {
+//			            type: 'column',
+//			            renderTo: 'are'
+//			        },
+//			        title: {
+//			            text: chartTitle
+//			        },
+//			        xAxis: {
+//			            type: "category",
+//						labels:{align: "center",enabled: true}
+//			        },
+//					yAxis: {
+//						type:"linear",
+//						labels:{enabled:true}	
+//					},
+//			        series: [{
+//			            data: processed_json
+//			        }],
+//					legend:{
+//						title:{
+//							text:" IP values "
+//						}
+//					}
+//					});
+//				}
+//				
+//
+//			    function PieChart() {
+//			        $('#are').highcharts({
+//			            chart: {
+//			                type: 'pie',
+//			                options3d: {
+//			                    enabled: true,
+//			                    alpha: 45,
+//			                    beta: 0
+//			                }
+//			            },
+//			            title: {
+//			                text: chartTitle
+//			            },
+//			            tooltip: {
+//			                pointFormat: '<b>{point.percentage:.1f}%</b>'
+//			            },
+//			            plotOptions: {
+//			                pie: {
+//			                    allowPointSelect: true,
+//			                    cursor: 'pointer',
+//			                    depth: 35,
+//			                    dataLabels: {
+//			                        enabled: true,
+//			                        format: '{point.name}'
+//			                    }
+//			                }
+//			            },
+//			            series: [{
+//			                data: processed_json
+//			            }]
+//			        });
+//			    }
+//				 
+//			   
+//
+//
+//			    
+//
+//			}
+//
+//
+//			function BlockIP() {
+//			    var ip = document.getElementById("block_ip").value;
+//
+//			    var xhr = new XMLHttpRequest();
+//
+//			    xhr.open("GET", "http://52.39.5.137:8090/blockIP?ip=" + ip, false);
+//			    xhr.send();
+//			    var JSONdata = xhr.responseText;
+//
+//			    alert(JSONdata);
+//
+//			}
+
+
+
+function openFirewall() {
 			    document.getElementById("details1").style.display = "block";
 			    document.getElementById("details2").style.display = "none";
 			}
@@ -13,7 +209,7 @@
 			function myFunction() 
 			{
 				var chartTitle;
-			    var common = "http://52.39.5.137:8090";
+			    var common = "http://localhost:8090";
 			    var e = document.getElementById("dropdown1");
 			    var f = document.getElementById("dropdown2");
 
@@ -31,10 +227,10 @@
 
 			    if (protocol != "") {
 			        url = common + "/" + plota + "/" + plotb + "?ip=" + protocol;
-					localStorage.setItem("url",url);
+					sessionStorage.setItem("url",url);
 			    } else {
 			        url = common + "/" + plota + "/" + plotb;
-					localStorage.setItem("url",url);
+					sessionStorage.setItem("url",url);
 			    }
 			    
 
@@ -102,33 +298,34 @@
 
 				BarChart();
 				
-//				document.getElementById("clickme").onclick = function(){
-//					PieChart();
-//				}
-//			
-
 				function BarChart() {
-					
-			    	var chart = new Highcharts.Chart({
-			        	chart: {
+			    var chart = new Highcharts.Chart({
+			        chart: {
 			            type: 'column',
-			            renderTo : 'are'
-			        	},
-						
-						title:	{
-							text: chartTitle
-						},
-			        	xAxis: {
-			            	type: "category"
-			        	},
-			        	series: [{
-			            	data: processed_json
-			        		}]
-			    		});
-
-
-
+			            renderTo: 'are'
+			        },
+			        title: {
+			            text: chartTitle
+			        },
+			        xAxis: {
+			            type: "category",
+						labels:{align: "center",enabled: true}
+			        },
+					yAxis: {
+						type:"linear",
+						labels:{enabled:true}	
+					},
+			        series: [{
+			            data: processed_json
+			        }],
+					legend:{
+						title:{
+							text:" IP values "
+						}
+					}
+					});
 				}
+
 				
 				
 				function PieChart(){
@@ -166,13 +363,7 @@
 
 			}
 
-			
 
-
-			
-
-
-//			 Blocking IP
 
 			 function BlockIP() {
 			     var ip = document.getElementById("block_ip").value;
